b11170e3df8fdbdce6473e3a3e65cc3511325742
lzsidek <laszlo.zsidek@outlook.com>
2025-02-19 21:16:36 +0100

    Clean-up ang logging

--- src/test/java/com/zsidek/utils/ResourceReader.java
+++ src/test/java/com/zsidek/utils/ResourceReader.java
@@ -1,4 +1,27 @@
 package com.zsidek.utils;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Slf4j
 public class ResourceReader {
+
+    public static JsonNode getJsonNode(String fileName) {
+        ClassLoader classLoader = ResourceReader.class.getClassLoader();
+        try (InputStream inputStream = classLoader.getResourceAsStream(fileName)) {
+            if (inputStream == null) {
+                throw new IllegalArgumentException("File not found: " + fileName);
+            }
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readTree(inputStream);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+        return null;
+    }
+
 }
